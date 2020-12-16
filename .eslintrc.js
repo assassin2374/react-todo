6ffd54aef7567c2fce93e26ece6ec71f54257e3d
assassin_box <assassin543@outlook.jp>
2020-12-16 23:20:24 +0900

    yarnに適応

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  parser: "babel-eslint",
+  plugins: ["react", "prettier"],
+  parserOptions: {
+    version: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier/react",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "prettier/prettier": "error",
+  },
+};
